24fe4d64e74d311ba3d08d0e16411601fda6f7ac
Larissa Dominique Garcia <larissadominique92@gmail.com>
2017-07-14 22:49:29 -0300

    Show the next negative balance day

--- index.php
+++ index.php
@@ -17,16 +17,50 @@
 </div>
 <!-- /.row -->
 
-<div class="row">
-	<div class="col-lg-12">
-		<div class="alert alert-danger alert-dismissable">
-			<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-			<i class="fa fa-exclamation-triangle"></i>  <strong>Atenção! </strong> O caixa ficará negativo em 4 dias!
-		</div>
-	</div>
-</div>
-<!-- /.row -->
+<?php
+$link = mysqli_connect('localhost', 'root', '', 'db_cash_flow');
+mysqli_set_charset($link,'utf8');
 
+date_default_timezone_set("America/Sao_Paulo");
+$date = date("Y-m-d");
+
+$sql = "select * from `tbl_balance` WHERE date >= '$date' && value < 0 ORDER BY date";
+
+// excecute SQL statement
+$result = mysqli_query($link,$sql);
+
+// die if SQL statement failed
+if (!$result) {
+    http_response_code(404);
+    die(mysqli_error());
+}
+
+if($row = mysqli_fetch_object($result)) {
+
+    $date = $row->date;
+
+    $day = substr($date, 8, 2);
+    $month = substr($date, 5, 2);
+    $year = substr($date, 0, 4);
+
+    $date = "$day/$month/$year";
+
+    ?>
+
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="alert alert-danger alert-dismissable">
+                <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+                <i class="fa fa-exclamation-triangle"></i>
+                <strong>Atenção! </strong>
+                O caixa ficará negativo em <?php echo($date); ?>!
+            </div>
+        </div>
+    </div>
+    <!-- /.row -->
+    <?php
+}
+?>
 <div class="row">
 	<div class="col-lg-12">
 		<div class="panel panel-default">
