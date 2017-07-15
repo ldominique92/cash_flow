f527773910dc3610030bcafa6a451f2c31b6e365
Larissa Dominique Garcia <larissadominique92@gmail.com>
2017-07-15 18:14:48 -0300

    Added timeliner to home

--- index.php
+++ index.php
@@ -1,19 +1,22 @@
-<?php 
-	include 'header.php';
+<?php
+include 'header.php';
 ?>
-
+<link href="<?php echo ($site_url); ?>css/plugins/datepicker/datepicker.less" rel="stylesheet/less">
+<script src="<?php echo ($site_url); ?>js/plugins/datepicker/bootstrap-datepicker.js"></script>
+<link href="<?php echo ($site_url); ?>css/plugins/jquery-timeliner.css" rel="stylesheet">
+<script src="<?php echo ($site_url); ?>js/plugins/jquery-timeliner.js"></script>
 <!-- Page Heading -->
 <div class="row">
-	<div class="col-lg-12">
-		<h1 class="page-header">
-			Bem vindo!
-		</h1>
-		<ol class="breadcrumb">
-			<li class="active">
-				<i class="fa fa-home"></i> Home
-			</li>
-		</ol>
-	</div>
+    <div class="col-lg-12">
+        <h1 class="page-header">
+            Bem vindo!
+        </h1>
+        <ol class="breadcrumb">
+            <li class="active">
+                <i class="fa fa-home"></i> Home
+            </li>
+        </ol>
+    </div>
 </div>
 <!-- /.row -->
 
@@ -62,17 +65,89 @@ if($row = mysqli_fetch_object($result)) {
 }
 ?>
 <div class="row">
-	<div class="col-lg-12">
-		<div class="panel panel-default">
-			<div class="panel-heading">
-				<h3 class="panel-title"><i class="fa fa-bar-chart-o fa-fw"></i> Area Chart</h3>
-			</div>
-			
-		</div>
-	</div>
+    <div class="col-lg-12">
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h3 class="panel-title"><i class="fa fa-money fa-fw"></i> Próximos lançamentos</h3>
+            </div>
+            <div class="panel-body tumeliner-container">
+                <div id="timeliner">
+                    <ul class="columns">
+                        <?php
+                        $date = date("Y-m-d");
+
+                        $sql = "SELECT p.description, t.description as type, p.money_signal, p.money_value, p.due_date, c.name as costumer ".
+                            "FROM tbl_postings p ".
+                            "INNER JOIN tbl_costumers c ON c.id = p.costumer ".
+                            "INNER JOIN tbl_posting_types t ON t.id = p.type ".
+                            " WHERE p.due_date >= '$date' ORDER BY p.due_date LIMIT 10".
+                            "";
+                        //echo($sql);
+
+                        // excecute SQL statement
+                        $result = mysqli_query($link,$sql);
+
+                        // die if SQL statement failed
+                        if (!$result) {
+                            http_response_code(404);
+                            die(mysqli_error());
+                        }
+
+                        while($row = mysqli_fetch_object($result)) {
+                            $due_date = $row->due_date;
+
+                            $day = substr($due_date, 8, 2);
+                            $month = substr($due_date, 5, 2);
+                            $year = substr($due_date, 0, 4);
+
+                            $due_date = "$day/$month/$year";
+                            ?>
+                            <li>
+                                <?php
+                                if($row->money_signal == "+") {
+                                ?>
+                                <div class="timeliner_element green">
+                                    <?php }
+                                    else {
+                                    ?>
+                                    <div class="timeliner_element bricky">
+                                        <?php
+                                        }
+                                        ?>
+                                        <div class="timeliner_title">
+                                    <span class="timeliner_label">
+                                        <?php echo($row->description); ?>
+                                    </span>
+                                            <span class="timeliner_date">
+                                        <?php echo($due_date); ?>
+                                    </span>
+                                        </div>
+                                        <div class="content">
+                                            <?php
+                                            echo ("R$ ".$row->money_signal.$row->money_value." ");
+                                            if($row->money_signal == "+") {
+                                                echo("recebido de");
+                                            }
+                                            else {
+                                                echo("pago a");
+                                            }
+                                            echo(" ".$row->costumer." como ".$row->type);
+                                            ?>
+                                        </div>
+                                    </div>
+                            </li>
+                            <?php
+                        }
+                        ?>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
+<script src="<?php echo ($site_url); ?>js/next-postings.js"></script>
 <!-- /.row -->
-<?php 
-	include 'footer.php';
+<?php
+include 'footer.php';
 ?>
             
